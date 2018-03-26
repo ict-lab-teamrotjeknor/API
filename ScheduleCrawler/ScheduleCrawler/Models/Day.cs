746faf18fa0451595e49d1d21e08157e3a08f1b8
Oscar Veldman <oveldman@gmail.com>
2018-03-26 13:14:21 +0200

    Move project

---
+++ ScheduleCrawler/ScheduleCrawler/Models/Day.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ScheduleCrawler.Models
+{
+    public class Day
+    {
+        public string Name { get; private set;  }
+        public List<Hour> Hours { get; set; }
+
+        public Day(string name)
+        {
+            this.Hours = new List<Hour>();
+            this.Name = name;
+            CreateHours();
+        }
+
+        private void CreateHours()
+        {
+            for (int i = 1; i < 16; i++)
+            {
+                this.Hours.Add(new Hour(i));
+            }
+        }
+    }
+}
