c777c8910ebbd051fc4d18f648ecb8875ffa8523
Oscar Veldman <oveldman@gmail.com>
2018-03-29 12:38:12 +0200

    Delete not needed

--- Process/Model/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Process.Model
-{
-    public class Account
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Comfirm { get; set; }
-    }
-}
