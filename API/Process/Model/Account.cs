10082723ddfce1a642d6589727d6ccf22809a48b
Oscar Veldman <oveldman@gmail.com>
2018-03-29 12:35:33 +0200

    move

---
+++ API/Process/Model/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Process.Model
+{
+    public class Account
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Comfirm { get; set; }
+    }
+}
