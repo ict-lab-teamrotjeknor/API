75cd7615cec670e60508a0c044e5b4cd1781a4ed
Oscar Veldman <oveldman@gmail.com>
2018-05-07 10:18:18 +0200

    Publish File V2

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+systemctl stop kestrel-api-ictlab.service
+
+rm -r API/obj
+git pull
+
+rm -r ../Publish
+
+cd API
+dotnet ef migrations add ictlabV6
+dotnet ef database update
+
+dotnet publish -o ../../Publish
+
+sudo systemctl start kestrel-api-ictlab.service
